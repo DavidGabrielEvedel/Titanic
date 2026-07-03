9d65875df75bea93f6171f3a0aa74a341a0de40f
DavidGabrielEvedel <siddharthsingh.azr@gmail.com>
2026-07-03 16:08:40 +0530

    Add preprocessing and features

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,3 +8,28 @@ df["Age"]=df["Age"].fillna(df["Age"].median())
 df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
 print("\nMissing Values After Cleaning:\n")
 print(df.isnull().sum())
+
+# Feature Engineering
+df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
+
+print(df[["SibSp", "Parch", "FamilySize"]].head())
+
+df["IsAlone"] = (df["FamilySize"] == 1).astype(int)
+
+print(df[["FamilySize", "IsAlone"]].head())
+df["Sex"] = df["Sex"].map({
+    "male": 0,
+    "female": 1
+})
+df = pd.get_dummies(
+    df,
+    columns=["Embarked"],
+    drop_first=True
+)
+df["Embarked_Q"] = df["Embarked_Q"].astype(int)
+df["Embarked_S"] = df["Embarked_S"].astype(int)
+print(df.head())
+print(df.info())
+df.to_csv("data/cleaned_titanic.csv", index=False)
+
+print("Cleaned dataset saved successfully!")
